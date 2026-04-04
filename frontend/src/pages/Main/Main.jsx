4352f1c23d323129a85805338f2e159400fe8c19
KirusaK <kirusaklimenko@gmail.com>
2026-04-04 23:57:42 +0100

    feat: implement BookingForm section with styles and icons

--- frontend/src/pages/Main/Main.jsx
+++ frontend/src/pages/Main/Main.jsx
@@ -1,11 +1,13 @@
 import { Header } from "../../widgets/Header/Header";
 import { Hero } from "../../widgets/Hero/Hero";
+import { BookingForm } from "../../widgets/BookingForm/BookingForm";
 
 export const Main = () => {
   return (
     <>
       <Header />
       <Hero />
+      <BookingForm />
     </>
   );
 };
