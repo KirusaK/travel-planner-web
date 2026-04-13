158a66e80796ec11353e61b4161e86eda0b80a3f
Sanya <12481@student.pansim.edu.pl>
2026-04-13 16:09:13 +0300

    feat: implement login

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { FindFlight } from "../pages/FindFlight/FindFlight";
 import { Home } from "../pages/Home/Home";
+import { LoginForm } from "../widgets/LoginForm/LoginForm";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/FindFlight" element={<FindFlight />} />
+      <Route path="/LoginForm" element={<LoginForm />} />
     </Routes>
   );
 };
---
+++ frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,5 @@
+import { LoginForm } from "../../widgets/LoginForm/LoginForm";
+
+export const Login = () => {
+  return <LoginForm />;
+};
