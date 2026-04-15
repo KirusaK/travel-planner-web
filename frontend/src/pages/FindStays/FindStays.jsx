3c009cb557a8df6cee94c36ccacdc04c2b5bb805
KirusaK <kirusaklimenko@gmail.com>
2026-04-16 00:15:12 +0100

    feat(pages): initialize FindStay page with Header and Hero section

---
+++ frontend/src/pages/FindStays/FindStays.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../../widgets/Header/Header";
+import { Hero } from "../../widgets/Hero-FindStays/Hero";
+
+export const FindStays = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
+};
