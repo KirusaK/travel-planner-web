e582dfc011e190cd131082ed2f94214cf3493df7
KirusaK <kirusaklimenko@gmail.com>
2026-04-16 00:39:57 +0100

    feat: complete StaySearch section

--- frontend/src/pages/FindStays/FindStays.jsx
+++ frontend/src/pages/FindStays/FindStays.jsx
@@ -1,11 +1,13 @@
 import { Header } from "../../widgets/Header/Header";
 import { Hero } from "../../widgets/Hero-FindStays/Hero";
+import { StaySearch } from "../../widgets/StaySearch/StaySearch";
 
 export const FindStays = () => {
   return (
     <>
       <Header />
       <Hero />
+      <StaySearch />
     </>
   );
 };
