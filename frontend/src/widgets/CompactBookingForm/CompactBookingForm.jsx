ad86f6fff2ac8903b8eba68b702d7c8e05938b28
KirusaK <kirusaklimenko@gmail.com>
2026-07-05 19:55:49 +0100

    feat: integrate Redux Toolkit for flights flow, implement dynamic search filtering, and link BookingForm to FlightCard

--- frontend/src/widgets/CompactBookingForm/CompactBookingForm.jsx
+++ frontend/src/widgets/CompactBookingForm/CompactBookingForm.jsx
@@ -1,24 +1,59 @@
 import { useState } from "react";
-import sprite from "../../shared/assets/icons/symbol-defs.svg";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setFlightSearch } from "../../app/store/reducer/flightSlice.js";
 import { TripSelect } from "../BookingForm/ui/TripSelect/TripSelect";
 import { PassengerSelect } from "../BookingForm/ui/PassengerSelect/PassengerSelect";
+import sprite from "../../shared/assets/icons/symbol-defs.svg";
 import styles from "./CompactBookingForm.module.scss";
 
 export const CompactBookingForm = () => {
-  const [tripType, setTripType] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // Локальные стейты
+  const [routeInput, setRouteInput] = useState(""); // Стейт для поля From - to
+  const [tripType, setTripType] = useState(""); // Стейт для поля Trip
+  const [dates, setDates] = useState(""); // Стейт для поля Depart -Return
   const [passengerType, setPassengerType] = useState({
     count: 1,
     class: "Economy",
-  });
+  }); // Стейт для поля Passenger - Class
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const [cityFrom, cityTo] = routeInput.split("-").map((str) => str.trim());
+
+    const searchPayload = {
+      from: cityFrom || "",
+      to: cityTo || "",
+      trip: tripType,
+      depart: dates,
+      return: "",
+      passenger: passengerType.count,
+      flightClass: passengerType.class,
+    };
+
+    dispatch(setFlightSearch(searchPayload));
+    navigate("/FlightsPage");
+
+    console.log("Данные отправлены в store: ", searchPayload);
+  };
 
   return (
     <section className={styles.wrapper}>
-      <form className={styles.wrapper_container}>
+      <form className={styles.wrapper_container} onSubmit={handleSubmit}>
         {/* Поле From-To */}
         <div className={styles.wrapper_from}>
           <div className={styles.wrapper_from_input}>
             <label className={styles.wrapper_from_label}>From - To</label>
-            <input type="text" className={styles.wrapper_from_input_field} />
+            <input
+              type="text"
+              className={styles.wrapper_from_input_field}
+              value={routeInput}
+              onChange={(e) => setRouteInput(e.target.value)}
+            />
             <svg width={24} height={24} className={styles.wrapper_from_icon}>
               <use href={sprite + "#icon-ion_swap-horizontal"}></use>
             </svg>
@@ -32,7 +67,12 @@ export const CompactBookingForm = () => {
             <label className={styles.wrapper_depart_label}>
               Depart- Return
             </label>
-            <input type="date" className={styles.wrapper_depart_input_field} />
+            <input
+              type="date"
+              className={styles.wrapper_depart_input_field}
+              value={dates}
+              onChange={(e) => setDates(e.target.value)}
+            />
           </div>
         </div>
         {/* Поле Passengers - Class */}
