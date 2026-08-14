18c457868f3aa21fcd786f9339d2ab509ceff79e
Oleksandr1404 <12481@student.pansim.edu.pl>
2026-08-15 02:00:05 +0300

    Create SignUp pages, and fix bugs

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -2,11 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import { FindFlight } from "../pages/FindFlight/FindFlight";
 import { Home } from "../pages/Home/Home";
 import { Login } from "../pages/Login/Login";
+import { SignUp } from "../pages/SignUp/SignUp";
 import { FindStays } from "../pages/FindStays/FindStays";
 import { FlightsPage } from "../pages/FlightsPage/FlightsPage";
 import { HotelPage } from "../pages/HotelPage/HotelPage";
 import { FlightDetailPage } from "../pages/FlightDetailPage/FlightDetailPage";
-import { SignUp } from "../pages/SignUp/SignUp";
 
 const App = () => {
   return (
@@ -14,11 +14,11 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/FindFlight" element={<FindFlight />} />
       <Route path="/Login" element={<Login />} />
+      <Route path="/SignUp" element={<SignUp />} />
       <Route path="/FindStays" element={<FindStays />} />
       <Route path="/FlightsPage" element={<FlightsPage />} />
       <Route path="/HotelPage" element={<HotelPage />} />
       <Route path="/FlightDetailPage" element={<FlightDetailPage />} />
-      <Route path="/SignUp" element={<SignUp />} />
     </Routes>
   );
 };
