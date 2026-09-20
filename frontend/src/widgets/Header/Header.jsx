9cd7adcb4d505db4b1da0092595457f96098f144
KirusaK <kirusaklimenko@gmail.com>
2026-09-20 21:39:20 +0100

    feat(header): add account dropdown menu and profile toggle logic

--- frontend/src/widgets/Header/Header.jsx
+++ frontend/src/widgets/Header/Header.jsx
@@ -3,10 +3,16 @@ import sprite from "../../shared/assets/icons/symbol-defs.svg";
 import { useAuth } from "../../shared/context/AuthContext.jsx";
 import defaultAvatar from "../../shared/assets/image/Ellipse 1.jpg";
 import styles from "./Header.module.scss";
+import { useState } from "react";
+import { MyAccountModal } from "../MyAccountModal/MyAccountModal.jsx";
 
 export const Header = ({ isTransparent, hasShadow }) => {
   const location = useLocation();
   const { user } = useAuth();
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = () => setModalOpen((prev) => !prev);
+  const handleModalClose = () => setModalOpen(false);
 
   const isFlightActive =
     location.pathname === "/FindFlight" ||
@@ -18,85 +24,99 @@ export const Header = ({ isTransparent, hasShadow }) => {
     location.pathname === "/FindStays" ||
     location.pathname.includes("/hotel-detail");
 
+  const isFavouriteActive =
+    location.pathname === "/Favourites";
+
   console.log("Current user in Header:", user);
 
   return (
-    <div
-      className={`${styles.header} ${isTransparent ? styles.transparent : ""} ${hasShadow ? styles.with_shadow : ""}`}
-    >
-      <div className={styles.header_links}>
-        <NavLink
-          to="/FindFlight"
-          className={
-            isFlightActive
-              ? `${styles.header_link} ${styles.active}`
-              : styles.header_link
-          }
-        >
-          <svg width={24} height={24}>
-            <use href={sprite + "#icon-airplane"}></use>
-          </svg>
-          <span className={styles.header_link_text}>Find Flight</span>
-        </NavLink>
-        <NavLink
-          to="/FindStays"
-          className={
-            isHotelActive
-              ? `${styles.header_link} ${styles.active}`
-              : styles.header_link
-          }
-        >
-          <svg width={24} height={24}>
-            <use href={sprite + "#icon-ion_bed"}></use>
-          </svg>
-          <span className={styles.header_link_text}>Find Stays</span>
-        </NavLink>
-      </div>
-      <div className={styles.header_logo}>
-        <Link to="/">
-          <svg width={110} height={36}>
-            <use href={sprite + "#icon-Logo"}></use>
-          </svg>
-        </Link>
-      </div>
-      {user ? (
-        <div className={styles.header_user_menu}>
-          <Link to="/" className={styles.header_favourites}>
-            <svg width={24} height={24}>
-              <use href={sprite + "#icon-heart"} />
-            </svg>
-
-            <span>Favorites</span>
-          </Link>
-
-          <div className={styles.header_divider}>|</div>
-
-          <div className={styles.header_profile}>
-            <img
-              src={defaultAvatar}
-              alt="User Avatar"
-              className={styles.header_avatar}
-            />
-
-            <span>
-              {user.firstName || user.lastName}{" "}
-              {user.firstName?.[0] || user.lastName?.[0]}.
-            </span>
-          </div>
-        </div>
-      ) : (
-        <div className={styles.header_button}>
-          <Link to="/Login" className={styles.header_button_text1}>
-            Login
-          </Link>
-          <Link
-            to="/SignUp"
-            className={`${styles.header_button_text2} ${styles.header_button_decoration}`}
+    <section>
+      <div
+        className={`${styles.header} ${isTransparent ? styles.transparent : ""} ${hasShadow ? styles.with_shadow : ""}`}
+      >
+        <div className={styles.header_links}>
+          <NavLink
+            to="/FindFlight"
+            className={
+              isFlightActive
+                ? `${styles.header_link} ${styles.active}`
+                : styles.header_link
+            }
           >
-            Sign Up
+            <svg width={24} height={24}>
+              <use href={sprite + "#icon-airplane"}></use>
+            </svg>
+            <span className={styles.header_link_text}>Find Flight</span>
+          </NavLink>
+          <NavLink
+            to="/FindStays"
+            className={
+              isHotelActive
+                ? `${styles.header_link} ${styles.active}`
+                : styles.header_link
+            }
+          >
+            <svg width={24} height={24}>
+              <use href={sprite + "#icon-ion_bed"}></use>
+            </svg>
+            <span className={styles.header_link_text}>Find Stays</span>
+          </NavLink>
+        </div>
+        <div className={styles.header_logo}>
+          <Link to="/">
+            <svg width={110} height={36}>
+              <use href={sprite + "#icon-Logo"}></use>
+            </svg>
           </Link>
         </div>
-      )}
-    </div>
+        {user ? (
+          <div className={styles.header_user_menu}>
+            <Link
+              to="/Favourites"
+              className={
+                isFavouriteActive
+                  ? `${styles.header_favourites} ${styles.active}`
+                  : styles.header_favourites
+              }
+            >
+              <svg width={24} height={24}>
+                <use href={sprite + "#icon-heart"} />
+              </svg>
+
+              <span>Favorites</span>
+            </Link>
+
+            <div className={styles.header_divider}>|</div>
+
+            <button className={styles.header_profile} onClick={toggleModal}>
+              <img
+                src={defaultAvatar}
+                alt="User Avatar"
+                className={styles.header_avatar}
+              />
+
+              <span>
+                {user.firstName || user.lastName}{" "}
+                {user.firstName?.[0] || user.lastName?.[0]}.
+              </span>
+            </button>
+          </div>
+        ) : (
+          <div className={styles.header_button}>
+            <Link to="/Login" className={styles.header_button_text1}>
+              Login
+            </Link>
+            <Link
+              to="/SignUp"
+              className={`${styles.header_button_text2} ${styles.header_button_decoration}`}
+            >
+              Sign Up
+            </Link>
+          </div>
+        )}
+      </div>
+
+      <MyAccountModal isOpen={isModalOpen} onClose={handleModalClose} />
+    </section>
   );
 };
